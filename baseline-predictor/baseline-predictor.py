01c7a0fade9ab61213e4a27c8b997581c32105da
KUrushi <36495149+KUrushi@users.noreply.github.com>
2026-02-23 00:10:16 +0900

    Update solution for baseline-predictor

--- baseline-predictor/baseline-predictor.py
+++ baseline-predictor/baseline-predictor.py
@@ -6,7 +6,8 @@ def baseline_predict(ratings_matrix, target_pairs):
     # Write code here
     user_mean = []
     item_mean = []
-    global_means = []
+    global_totals = 0
+    global_counts = 0
 
     item_counts = len(ratings_matrix[0])
     items_scores = {
@@ -21,17 +22,19 @@ def baseline_predict(ratings_matrix, target_pairs):
             if user_ratings[i] != 0:
                 total += user_ratings[i]
                 effective_rating_counts += 1
-                global_means.append(user_ratings[i])
-
                 items_scores['total_ratings'][i] += user_ratings[i]
                 items_scores['effective_counts'][i] += 1
+
+        global_totals += total
+        global_counts += effective_rating_counts
         user_mean.append(total / effective_rating_counts)
 
+    
     item_mean = [items_scores['total_ratings'][i]/items_scores['effective_counts'][i]
                 for i in range(item_counts)]
 
                 
-    global_means = sum(global_means) / len(global_means)
+    global_means = global_totals / global_counts
 
     outputs = []
     for u, i in target_pairs:
